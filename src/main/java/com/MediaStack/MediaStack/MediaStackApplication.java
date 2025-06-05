9d10e81da7ca34356cdca058e8075d1f88f74ba9
Lucy Hühn <666nikkineko@gmail.com>
2025-06-05 17:24:20 +0200

    refactor / MediaStack has its own Repository now TODO: Copy commit history

---
+++ src/main/java/com/MediaStack/MediaStack/MediaStackApplication.java
@@ -0,0 +1,11 @@
+package com.MediaStack.MediaStack;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class MediaStackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MediaStackApplication.class, args);
+    }
+}
